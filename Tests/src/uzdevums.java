fb3c501e298da1f9f40423121e1604c4e8f0b88f
janislaugalis <laugalis3211@gmail.com>
2021-06-17 16:45:09 +0300

    Definē mainīgos, pievieno scanner

--- Tests/src/uzdevums.java
+++ Tests/src/uzdevums.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 
 public class uzdevums {
 
 	public static void main(String[] args) {
-		System.out.println("s");
+		int x=0;
+		int y;
+		int x1=0, x2=0, x3=0, x4=0, x5=0, x6=0, x7=0, x8=0, x9=0, x10=0;
+		int punkti=10;
+		Scanner scan = new Scanner(System.in);
 
 	}
 
