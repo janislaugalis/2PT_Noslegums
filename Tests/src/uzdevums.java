7c3dd71175f354fc18ed5b953c625596639a2d70
janislaugalis <laugalis3211@gmail.com>
2021-06-17 17:03:06 +0300

    testa jautājumu izveide

--- Tests/src/uzdevums.java
+++ Tests/src/uzdevums.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class uzdevums {
 
@@ -8,6 +9,17 @@ public class uzdevums {
 		int x1=0, x2=0, x3=0, x4=0, x5=0, x6=0, x7=0, x8=0, x9=0, x10=0;
 		int punkti=10;
 		Scanner scan = new Scanner(System.in);
+		
+		do{ // jautâjumu sazarojums
+			x = Integer.parseInt(JOptionPane.showInputDialog("Kâdâs situâcijâs izmanto If Else sazarojumu? \n1. Ja ir vairâkas situâcijas. \n2. Ja programmâ ir kïûdas.  \n3. Ja programma pieprasa semikolu \n4. Ja programmâ ir ievietots “Scanner” ")); // nosacîjums
+			y = 1; //atbilde
+			if (x==y) {
+				JOptionPane.showMessageDialog(null, "--- Pareizi ---");
+			} else {
+				JOptionPane.showMessageDialog(null, "--- Nepareizi, mçìini vçlreiz ---");
+				x1 = 1;
+			}
+		}while(x!=y);
 
 	}
 
